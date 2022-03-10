ad0b73ed29bf3f6700bd5d1942d60a8396c52eac
maindolaamit <maindola.amit@gmail.com>
2022-03-10 20:22:43 +0530

    moved menu-items to component

--- src/components/directory-menu/directory-menu.jsx
+++ src/components/directory-menu/directory-menu.jsx
@@ -1,16 +1,21 @@
+import { useState } from 'react';
 import './directory-menu.style.css'
 import MenuItem from '../../components/menu-items/menu-items';
 
 const DirectoryMenu = () => {
-    
+    const [section, setSection] = useState([
+        { id: 1, title: 'hats', imageUrl: 'https://i.ibb.co/cvpntL1/hats.png' },
+        { id: 2, title: 'jackets', imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png' },
+        { id: 3, title: 'sneakers', imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png' },
+        { id: 4, title: 'womens', imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png', size: 'large' },
+        { id: 5, title: 'mens', imageUrl: 'https://i.ibb.co/R70vBrQ/mens.png', size: 'large' }
+    ]);
 
     return (
         <div className="directory-menu">
-            <MenuItem title="HATS" />
-            <MenuItem title="SNEAKERS" />
-            <MenuItem title="HATS" />
-            <MenuItem title="MEN'S" />
-            <MenuItem title="WOMEN'S" />
+            {section.map(({ title, imageUrl, id, size }) => {
+                return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+            })}
         </div>
     );
 };
--- src/components/menu-items/menu-items.jsx
+++ src/components/menu-items/menu-items.jsx
@@ -1,12 +1,18 @@
-import './menu-items.style.css';
+import './menu-items.style.scss';
 
-const MenuItem = ({ title }) => {
-    return (<div className="menu-item">
-        <div className="content">
-            <h1 className="title">{title}</h1>
-            <span className="subtitle">SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div className={`${size} menu-item`}>
+        <div
+            className='background-image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
+        <div className='content'>
+            <h1 className='title'>{title.toUpperCase()}</h1>
+            <span className='subtitle'>SHOP NOW</span>
         </div>
-    </div>);
-}
+    </div>
+);
 
 export default MenuItem;
