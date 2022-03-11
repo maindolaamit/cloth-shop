d51a57cf07c65d679542a511b5268a33205d6a6a
maindolaamit <maindola.amit@gmail.com>
2022-03-11 12:34:57 +0530

    added react-router-dom and implemented a sample routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-import HomePage from './pages/home-page/homepage';
+import "./App.css";
+import HomePage from "./pages/home-page/homepage";
+import { Routes, Navigate, Route } from "react-router-dom";
+
+const HatPage = () => {
+  return <h2>Hats Page</h2>;
+};
 
 function App() {
   return (
-    <div >
-      <HomePage />
+    <div>
+      {/* react-router-dom@6 --> Switch has been replaced with Routes and Route take JSX element*/}
+      <Routes>
+        {/* react-router-dom@6 --> Redirect has been replaced with Navigate  */}
+        <Route path="/" element={<Navigate replace to="/home" />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/hats" element={<HatPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/directory-menu/directory-menu.jsx
+++ src/components/directory-menu/directory-menu.jsx
@@ -1,23 +1,46 @@
-import { useState } from 'react';
-import './directory-menu.style.css'
-import MenuItem from '../../components/menu-items/menu-items';
+import { useState } from "react";
+import "./directory-menu.style.css";
+import MenuItem from "../../components/menu-items/menu-items";
 
 const DirectoryMenu = () => {
-    const [section, setSection] = useState([
-        { id: 1, title: 'hats', imageUrl: 'https://i.ibb.co/cvpntL1/hats.png' },
-        { id: 2, title: 'jackets', imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png' },
-        { id: 3, title: 'sneakers', imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png' },
-        { id: 4, title: 'womens', imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png', size: 'large' },
-        { id: 5, title: 'mens', imageUrl: 'https://i.ibb.co/R70vBrQ/mens.png', size: 'large' }
-    ]);
+  const [section, setSection] = useState([
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      linkUrl: "hats",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      size: "large",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/mens.png",
+      size: "large",
+    },
+  ]);
 
-    return (
-        <div className="directory-menu">
-            {section.map(({ title, imageUrl, id, size }) => {
-                return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-            })}
-        </div>
-    );
+  return (
+    <div className="directory-menu">
+      {section.map(({ id, ...otherProps }) => {
+        return <MenuItem key={id} {...otherProps} />;
+      })}
+    </div>
+  );
 };
 
 export default DirectoryMenu;
