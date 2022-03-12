c66585331acefc3b81bf2457f5d47067a080e28f
maindolaamit <maindola.amit@gmail.com>
2022-03-12 19:13:03 +0530

    implemented createProfileDocument to maintain user profile

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import HomePage from "./pages/home-page/homepage";
 import ShopPage from "./pages/shop-page/shoppage";
 import Header from "./components/header/header";
 import SingInAndSignUpPage from "./pages/singn-in-and-sing-up/singn-in-and-sign-up";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [authUser, setAuthUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null);
   useEffect(
     () => {
-      const unregisterAuthObserver = auth.onAuthStateChanged((user) => {
-        setAuthUser(user);
+      const unregisterAuthObserver = auth.onAuthStateChanged(async (user) => {
+        if (user) {
+          const userRef = createUserProfileDocument(user);
+          (await userRef).onSnapshot((snapShot) => {
+            setCurrentUser({ id: snapShot.id, ...snapShot.data() });
+          });
+        }
+        console.log(currentUser);
         // Now you either return just unregisterAuthObserver
         // which will be called when the component is unmounted
       });
@@ -23,7 +29,7 @@ function App() {
 
   return (
     <div>
-      <Header authUser={authUser} />
+      <Header authUser={currentUser} />
       {/* react-router-dom@6 --> Switch has been replaced with Routes and Route take JSX element*/}
       <Routes>
         {/* react-router-dom@6 --> Redirect has been replaced with Navigate  */}
